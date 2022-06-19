d9a8758433133e95ddcbca0c8d278be39cf0b696
caro-oviedo <carolina.u.oviedo@gmail.com>
2022-06-19 20:54:14 -0300

    added double or nothing

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 const doubleBtn = document.getElementById("doubleBtn")
 
-
 function showDisplayButton(){
     rollBtn.style.display ="none"
     resetBtn.style.display = "block"
+    doubleBtn.style.display= "none"
 }
 
 function randomRoll(){
@@ -22,35 +22,41 @@ function randomRoll(){
 }
 
 function randomRollDoubleOrZero(){
-    return Math.floor(Math.random()* 7) 
+    return randomNumberDouble = Math.floor(Math.random()* 7) 
+    console.log(randomNumberDouble)
 }
 
+function player1DisplayScore(randomNumber){
+    player1Dice.textContent = randomNumber
+    player1Scoreboard.textContent = (player1Score += randomNumber)
+    player1Dice.classList.remove("active")
+    player2Dice.classList.add("active")
+    message.textContent =  "Player 2's turn"
+}
+
+function player2DisplayScore(randomNumber){
+    player2Dice.textContent = randomNumber
+    player2Scoreboard.textContent = (player2Score += randomNumber)
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+    message.textContent =  "Player 1's turn"
+}
 
 doubleBtn.addEventListener("click", function(){
-    let randomNumber = randomRollDoubleOrZero() * 2
-    console.log(randomNumber)
-    
+    let randomNumber1 = randomRollDoubleOrZero()
+    let randomNumber = randomNumber1 * 2
+    if (player1Turn){
+       player1DisplayScore(randomNumber)
+       
+    }
+    else {
+        player2DisplayScore(randomNumber)
+
+    }
+    checkResultAndPlayerTurn()
 })
 
-rollBtn.addEventListener("click", function(){
-const  randomNumber = randomRoll()
-
-    if (player1Turn) {    
-        player1Dice.textContent = randomNumber
-        player1Scoreboard.textContent = (player1Score += randomNumber)
-        player1Dice.classList.remove("active")
-        player2Dice.classList.add("active")
-        message.textContent =  "Player 2's turn"
-        
-    }
-    else{
-        player2Dice.textContent = randomNumber
-        player2Scoreboard.textContent = (player2Score += randomNumber)
-        player2Dice.classList.remove("active")
-        player1Dice.classList.add("active")
-        message.textContent =  "Player 1's turn"
-    }
-
+function checkResultAndPlayerTurn(){
     if (player1Score >= 20){
         message.textContent = "Player1 has won "
         showDisplayButton()
@@ -59,8 +65,18 @@ const  randomNumber = randomRoll()
         message.textContent = "Player 2 has won "
         showDisplayButton()
     }
-    
     player1Turn = !player1Turn
+}
+
+rollBtn.addEventListener("click", function(){
+const  randomNumber = randomRoll()
+if (player1Turn) {
+    player1DisplayScore(randomNumber)
+}
+else{
+    player2DisplayScore(randomNumber)
+    }
+   checkResultAndPlayerTurn()
 })
 
 
@@ -80,6 +96,7 @@ function reset(){
     player1Turn = true
     resetBtn.style.display = "none"
     rollBtn.style.display = "block"
+    doubleBtn.style.display= "block"
     player2Dice.classList.remove("active")
     player1Dice.classList.add("active")
 }
